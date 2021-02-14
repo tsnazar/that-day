b1c45f8a22f67f26f2ff51ec10d58e735cb679b9
tsnazar <tsnazr3@gmail.com>
2021-02-15 00:05:14 +0200

    first commit

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,37 +1,37 @@
 $(function() {
     $(window).on('load', function() {       
-        $(document).ready(function () {
-            $('#container').removeClass('hidden');
-            $('#loader').addClass('hidden'); 
-            $('#menu').click(function(){
-                if($('#sidebar').hasClass('animate-in')){
-            
-                    $('#sidebar').removeClass('animate-in');
-                    $('#sidebar').addClass('animate-out');
-                    // $('#sidebar').addClass('hidden');
-                    
-                    
-                  } else {
-                    // $('#sidebar').removeClass('hidden');
-                    $('#sidebar').addClass('animate-in');
-                    $('#sidebar').removeClass('animate-out');
-                    
-                  }
-            });
+        
+        $('#container').removeClass('hidden');
+        $('#loader').addClass('hidden'); 
 
-            $('#articles').find('div').find('a').children('div .group').each(function(index){
-                var p = $(this).find('p');
-                var h = $(this).find('h2');
-
-                if($(this).height()-p.height()<$(this).height()/1.5){
-                    h.addClass('top-4 lg:top-1/3 lg:group-hover:top-1/3');
-                    p.addClass('top-16 lg:top-1/2 lg:group-hover:top-1/2');
+        $('#menu').click(function(){
+            if($('#sidebar').hasClass('animate-in')){
+        
+                $('#sidebar').removeClass('animate-in');
+                $('#sidebar').addClass('animate-out');
+                // $('#sidebar').addClass('hidden');
+                
+                
                 } else {
-                    h.addClass('top-8 lg:top-1/3  lg:group-hover:top-1/3');
-                    p.addClass('top-1/2 lg:top-1/2  lg:group-hover:top-1/2');
+                // $('#sidebar').removeClass('hidden');
+                $('#sidebar').addClass('animate-in');
+                $('#sidebar').removeClass('animate-out');
+                
                 }
-
-            }); 
         });
+
+        $('#articles').find('div').find('a').children('div .group').each(function(index){
+            var p = $(this).find('p');
+            var h = $(this).find('h2');
+
+            if($(this).height()-p.height()<$(this).height()/1.5){
+                h.addClass('top-4 lg:top-1/3 lg:group-hover:top-1/3');
+                p.addClass('top-16 lg:top-1/2 lg:group-hover:top-1/2');
+            } else {
+                h.addClass('top-8 lg:top-1/3  lg:group-hover:top-1/3');
+                p.addClass('top-1/2 lg:top-1/2  lg:group-hover:top-1/2');
+            }
+
+        }); 
     });
 });
